f25ee8cc453640f08c27850b390cf83bee8150f9
subseven <daniel@subseven.io>
2019-05-24 14:47:56 -0500

    Changed font awesome for LineAwesome CDN

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
   <link rel="stylesheet" type="text/css" href="css/normalize.css">
   <!-- The custom style sheet -->
   <link rel="stylesheet" type="text/css" href="css/styles.css">
-  <!-- Adds FontAwesome Icons CDN -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+  <!-- Adds LineAwesome Icons CDN -->
+  <link rel="stylesheet" href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome-font-awesome.min.css">
   <!-- Adds programming languge icons -->
   <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css">
   <!-- The scripts for this page -->
