343f8155f7fb1e606e6c6edbe2f167c882164e3c
subseven <daniel@subseven.io>
2019-06-10 16:14:56 -0500

    More

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <!-- Made by Daniel Hernandez :D You should hire me. -->
+
+<?php // TODO: ADD ALL OF THIS STUFF TO THE MAIN TEMPLATE: Documentation, switch up the container class ?>
+
 <html>
 <head>
   <meta charset="utf-8">
@@ -57,9 +60,7 @@
     Below this, you can replace the contents with your own links.
     -->
     <a href="javascript:void(0)" class="close-btn" onclick="closeNav()">&times;</a>
-    <a class="menu-item" href="#" class="btn" target="#"><i class="fa fa-home"></i> Home</a></li>
     <a class="menu-item" href="https://linkedin.com/in/subseven" class="btn" target="_blank" rel="noreferrer noopener"><i class="fa fa-linkedin"></i> LinkedIn</a></li>
-    <a class="menu-item" href="#" class="btn" target="_blank" rel="noreferrer noopener"><i class="fa fa-wordpress"></i> Blog</a></li>
     <a class="menu-item" href="https://twitter.com/SanKahlo" class="btn" target="_blank" rel="noreferrer noopener"><i class="fa fa-twitter"></i> Twitter</a></li>
     <a class="menu-item" href="https://github.com/subseven57" class="btn" target="_blank" rel="noreferrer noopener"><i class="fa fa-github"></i> GitHub</a></li>
     <!-- Refer to the documentation to see what icons are supported.
@@ -75,7 +76,7 @@
     <img src="/img/dan.png" id="user-pic" alt="Daniel Hernandez Image" />
     <h6 id="pre-desc">Sleep deprived student passionate about web development, social impact, music, and FLOSS.</span></h6>
     <!-- Your summary -->
-    <p id="post-desc">Your Summary.</p>
+    <p id="post-desc">I've always been a tech kid at heart.</p>
     <!-- Your mission -->
   </div>
   <!-- End of resume pre-container. -->
@@ -94,18 +95,19 @@
           <li><i class="fa fa-code-fork"></i> <strong>HTML</strong>: Experienced </li>
           <li><i class="fa fa-code-fork"></i> <strong>CSS</strong>: Experienced</li>
           <li><i class="fa fa-code-fork"></i> <strong>JavaScript</strong>: Experienced</li>
-          <li><i class="fa fa-code-fork"></i> <strong>PHP</strong>: Comfortable</li>
+          <li><i class="fa fa-code-fork"></i> <strong>PHP</strong>: Experienced</li>
           <li><i class="fa fa-code-fork"></i> <strong>Python</strong>: Comfortable</li>
           <li><i class="fa fa-code-fork"></i> <strong>Ruby/Rails</strong>: Somewhat Comfortable</li>
-          <li><i class="fa fa-code-fork"></i> <strong>Java</strong>: Would Rather Not</li>
         </ul>
 
         <h4 class="name-c"> Tools </h4>
         <ul>
           <!-- Refer to this for documentation: https://icons8.com/line-awesome/cheatsheet -->
           <li><i class="fa fa-star"></i> <strong>GNU/Linux</strong>: Very Experienced</li>
+          <li><i class="fa fa-star"></i> <strong>Git</strong>: Very Experienced</li>
           <li><i class="fa fa-star"></i> <strong>Sound Design/Editing</strong>: Experienced</li>
           <li><i class="fa fa-star"></i> <strong>CMS Management</strong>: Experienced</li>
+          <li><i class="fa fa-star"></i> <strong>Bootstrap</strong>: Experienced</li>
           <li><i class="fa fa-star"></i> <strong>Google Ads Platform</strong>: Experienced</li>
           <li><i class="fa fa-star"></i> <strong>Photo/Vector Editing</strong>: Comfortable</li>
           <li><i class="fa fa-star"></i> <strong>Video Editing</strong>: Somewhat Experienced</li>
@@ -117,10 +119,12 @@
         <h4 class="name-c"> Projects</h4>
         <ul>
           <!-- If you want or need to, you can add icons to programming languages here. Font Awesome has some missing -->
-          <li><a href="https://uroneiu.com" class="#" target="_blank" rel="noreferrer noopener"><i class="fa fa-wordpress"></i><strong>URO @ NEIU</strong></a> | Theme Design, Maintenance</li>
-          <li><a href="https://anklenfoot.com" class="#" target="_blank" rel="noreferrer noopener"><i class="fa fa-buysellads"></i><strong>Ankle N Foot</strong></a> | Marketing, Analytics</li>
+          <li><a href="https://uroneiu.com" class="#" target="_blank" rel="noreferrer noopener"><i class="fa fa-wordpress"></i><strong>URO @ NEIU</strong></a> | Infrastructure, Theme Dev, Maintenance</li>
+          <li><a href="https://anklenfoot.com" class="#" target="_blank" rel="noreferrer noopener"><i class="fa fa-buysellads"></i><strong>Ankle N Foot</strong></a> | Marketing, Analytics, SEO</li>
           <li><a href="https://github.com/SubSeven57/sky-website-template" class="#" target="_blank" rel="noreferrer noopener"><i class="fa fa-github"></i><strong>Sky Template</strong></a> | Code</li>
           <li><a href="https://theardentband.com/" class="#" target="_blank" rel="noreferrer noopener"><i class="fa fa-html5"></i><strong>The Ardent</strong></a> | Design, Code</li>
+          <li><a href="https://dreambigkid.com/" class="#" target="_blank" rel="noreferrer noopener"><i class="fa fa-wordpress"></i><strong>Dream Big, Kid</strong></a> | Design, Maintenance</li>
+
         </ul>
       </div>
     </div>
@@ -128,16 +132,30 @@
     <!-- Begin experience container-->
     <h2 class="header-c">Experience</h2>
     <div class="body-c">
-      <h4 class="name-c">Title at Job</h4> <span class="it"> From xxxx to xxxx</span>
+
+      <h4 class="name-c">Marketing Assistant at Ankle N Foot</h4> <span class="it"> From Nov 2018 to Present</span>
       <hr class="divider">
-      <p class="blurb-c">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure
-        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <h4 class="name-c">Title at Job</h4> <span class="it"> From xxxx to xxxx</span>
+      <p class="blurb-c">Leading digital marketing initiatives at Ankle N Foot. Most of my work centers around balancing ad campaigns as well as coming up with organic ways
+      to increase their clientele. I also assisted with the launch of their new website by migrating a lot of their old infrastructure.</p>
+
+      <h4 class="name-c">Web Developer at SubSeven Designs</h4> <span class="it"> From Aug 2017 to Present</span>
       <hr class="divider">
-      <p class="blurb-c">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure
-        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p class="blurb-c">Created a series of websites for small business owners around the Chicago area. Assisted either in design, deployment, or a combination of both.</p>
+
+      <h4 class="name-c">Social Impact Program Coordinator at GiHub</h4> <span class="it"> From Jun 2017 to Aug 2017</span>
+      <hr class="divider">
+      <p class="blurb-c"> Developed a survey and email campaign for GitHub nonprofit customers. We wanted to understand how nonprofits and how they used open-source.
+      The survey went out to 25,000 accounts. <br> Then, the results were interpreted and analyzed survey information with R, Python, Looker, and SQL. The results of this survey were then compiled into a report and published.</p>
+
+      <h4 class="name-c">J. S. The Exchange</h4> <span class="it"> From Nov 2015 to May 2017</span>
+      <hr class="divider">
+      <p class="blurb-c">Assisted as sales rep with the maintenance and upkeep of our locations.</p>
+
+      <h4 class="name-c">Instructor at iD Tech</h4> <span class="it"> From Jun 2014 to Aug 2016 (Seasonal)</span>
+      <hr class="divider">
+      <p class="blurb-c"></p>
+
+
     </div>
     <!-- End experience container -->
 
@@ -158,7 +176,7 @@
   <!-- Begin contact container-->
   <h2 class="header-c">Contact Me</h2>
   <div class="body-c">
-    <form action="https://formspree.io/subseven.me@gmail.com" method="POST">
+    <form action="https://formspree.io/subseven.me@gmail.com" method="POST" id="contactMe">
       <div>
         <label for="name">Name:</label>
         <input type="text" id="name" name="name">
@@ -169,11 +187,12 @@
       </div>
       <div>
         <label for="msg">Message:</label>
-        <textarea id="msg" name="message"></textarea>
+        <textarea name="message"></textarea>
       </div>
       <div class="form-button">
         <button type="submit">Send your message</button>
       </div>
+      <div><em>You will be redirected after submission. I am using FormSpree for validation.</em></div>
     </form>
 
   </div>
